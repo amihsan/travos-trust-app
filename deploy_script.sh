e61221cc8f60dfb050f631d99bd25fdb8c9a23c3
amihsan <rumi.apece.du@gmail.com>
2023-12-30 18:16:25 +0100

    Updated main.yml

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+sudo su
 sudo yum update
 
+cd travos-trust-app/
+git pull https://github.com/amihsan/travos-trust-app.git
 # Update the React app
 sudo cd /home/ec2-user/travos-trust-app/frontend
 npm install
@@ -11,9 +14,6 @@ sudo cd /home/ec2-user/travos-trust-app/backend
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-
-# Restart Gunicorn (Flask)
-sudo systemctl restart gunicorn
 
 # Restart Frontend Service
 sudo systemctl restart frontend
@@ -26,3 +26,6 @@ sudo systemctl restart nginx
 
 # Restart MongoDB Service
 sudo systemctl restart mongod
+
+# Restart Gunicorn (Flask)
+sudo systemctl restart gunicorn
