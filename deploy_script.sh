c6fdbb1d5fd57240134a28264e67c9fcbd0bf90e
amihsan <rumi.apece.du@gmail.com>
2023-12-30 17:38:49 +0100

    Updated main.yml

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,13 +4,22 @@ sudo su
 
 sudo yum update
 
+ls
+
+cd travos-trust-app/
+
+ls
+
 # Update the React app
-cd /home/ec2-user/travos-trust-app/frontend
+cd frontend/
+ls
 npm install
 npm run build
 
+cd ..
+
 # Update the Flask API
-cd /home/ec2-user/travos-trust-app/backend
+cd backend/
 python3 -m venv venv       
 source venv/bin/activate     
 pip install -r requirements.txt
