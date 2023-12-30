94e7e0fdcba97996137e8d59552390809bba1d81
amihsan <rumi.apece.du@gmail.com>
2023-12-30 23:34:22 +0100

    Ci/Cd

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
+
+# Run subsequent commands with elevated privileges
 sudo su
+
+# Update the system
 sudo yum update
 
-# Project Root
-cd travos-trust-app/
+# Change to the project directory
+cd /home/ec2-user/travos-trust-app/
 
-# Log
+# Pull the latest changes from the GitHub repository
 git pull https://github.com/amihsan/travos-trust-app.git
-echo "Updating changes from Github Repo"
 
-# Log
-echo "Updating the React app..."
-cd /home/ec2-user/travos-trust-app/frontend
+# Check the exit status of git pull
+if [ $? -eq 0 ]; then
+    echo "Git pull successful."
+else
+    echo "Git pull failed."
+    # You might want to exit the script or handle the failure accordingly
+    exit 1
+fi
+
+
+# Update the React app
+cd frontend
 npm install
 npm run build
 
+# Update the Flask API
+cd ../backend
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 
 # Restart Frontend Service
 sudo systemctl restart frontend
-echo "Frontend service restarted successfully."
+
+# Restart Backend Service
+sudo systemctl restart backend
 
 # Restart Nginx
 sudo systemctl restart nginx
-echo "Nginx restarted successfully."
 
 # Restart MongoDB Service
 sudo systemctl restart mongod
-echo "MongoDB service restarted successfully."
 
 # Restart Gunicorn (Flask)
 sudo systemctl restart gunicorn
-echo "Gunicorn restarted successfully."
