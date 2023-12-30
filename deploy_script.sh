5407b2f1faa030fb68b9d12e61487bf480d131b3
amihsan <rumi.apece.du@gmail.com>
2023-12-30 10:50:08 +0100

    CI/CD (Github Actions)

---
+++ deploy_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo su
+
+sudo yum update
+
+# Update the React app
+cd /home/ec2-user/travos-trust-app/frontend
+npm install
+npm run build
+
+# Update the Flask API
+cd /home/ec2-user/travos-trust-app/backend
+python3 -m venv venv       
+source venv/bin/activate     
+pip install -r requirements.txt
+
+# Restart Gunicorn (Flask)
+systemctl restart gunicorn
+
+# Restart Frontend Service
+systemctl restart frontend
+
+# Restart Backend Service
+systemctl restart backend
+
+# Restart MongoDB Service
+systemctl restart mongod
+
+# Restart Nginx
+systemctl restart nginx
