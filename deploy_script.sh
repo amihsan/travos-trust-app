dc4afd962a56de69b1cab37d6f1bb5220a61b7b5
amihsan <rumi.apece.du@gmail.com>
2023-12-31 10:48:55 +0100

    CI/CD Test

--- deploy_script.sh
+++ deploy_script.sh
@@ -9,11 +9,11 @@ sudo yum update
 # Change to the project directory
 cd /home/ec2-user/travos-trust-app/
 
-# Change ownership to ec2-user
-sudo chown -R ec2-user /home/ec2-user/travos-trust-app
-
 # Add an exception for the directory to resolve the "dubious ownership" issue
 git config --global --add safe.directory /home/ec2-user/travos-trust-app
+
+# Change ownership to ec2-user
+sudo chown -R ec2-user /home/ec2-user/travos-trust-app
 
 # Pull the latest changes from the GitHub repository
 git pull origin main
