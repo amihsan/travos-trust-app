bcd524877736248bc62098304874f76efc8ed0eb
amihsan <rumi.apece.du@gmail.com>
2024-01-07 16:39:25 +0100

    CI/CD Test

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -129,7 +129,7 @@ const Body = () => {
               className={`${styles.tooltipLink} ${styles.tooltipLinkTeal}`}
               onClick={handleFullDetailsClick}
             >
-              (<strong>SEE more details</strong>)
+              (<strong>see more details</strong>)
             </span>
           </p>
         </div>
