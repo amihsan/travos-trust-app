7541f425f6de61e164de8797f948c3df3c0d0e5b
amihsan <rumi.apece.du@gmail.com>
2024-04-06 15:04:09 +0200

    backend code refactored

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
-from main_routes import bp
+from routes.main_routes import bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(bp)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
     
