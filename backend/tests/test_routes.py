14e7b46fca7d5b1a7720df957f29b29495e99f47
amihsan <rumi.apece.du@gmail.com>
2024-04-06 18:16:01 +0200

    unit test (pytest) added

---
+++ backend/tests/test_routes.py
@@ -0,0 +1,15 @@
+# tests/test_routes.py
+
+from app import app
+
+def test_index_route():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_template_rendering():
+    client = app.test_client()
+    response = client.get('/')
+    assert b"Hello From Travos lab" in response.data
+
+        
