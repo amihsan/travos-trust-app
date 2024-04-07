3409cfd6028f5a526b03e69f713e6732290d9ff8
amihsan <rumi.apece.du@gmail.com>
2024-04-07 17:40:46 +0200

    aws deployment

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -6,6 +6,7 @@ def test_index_route():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
+    
 
 def test_template_rendering():
     client = app.test_client()
